98d99a8b70c6e3364620a7e751c397ab63b70e06
cassia <cascrisbern@gmail.com>
2019-07-01 14:09:07 +0100

    css changes and javascript added

--- script.js
+++ script.js
@@ -1,32 +1,83 @@
-$(document).ready(function(){
-   var formGroups = $(".form-group");
-   
+$( document ).ready( function () {
 
-   formGroups.each(function () {
-    var formGroup = $(this);
-    var label = formGroup.find('label');
-    var input = formGroup.find('input');    
+    $( "a" ).on( "click", function ( event ) {
+    
+            var hash = this.hash;
+            console.log('this is, see below');
+            console.log(this);
+            console.log('this.hash is see below');
+            console.log(this.hash);
 
-    function checkInput(){
+            if ( hash !== "" ) {
+                event.preventDefault();
+// hash is '#about'
+var mySelection = $(hash);
+console.log(mySelection);
+                var myTop;
+
+                if (hash == '#about') {
+                    // if we clicked on about, we scroll to 250px from the top, which is the same as the profile image distance from top
+                    myTop = 0;
+                } else {
+                    // otherwise, if for example hash is '#contact', we scroll to the top of the element with id "contact"
+                    myTop = $(hash).offset().top;
+                }
+
+               $("html, body").animate(
+                   {
+                       scrollTop: myTop
+                   }, 700, function(){
+                       console.log("last function executed");
+                   }
+               );
         
-        var valueLength = input.val().length;
-        console.log(messageLength);
-       
-        console.log("input value is: " + valueLength);
+            }
 
-        if (valueLength > 0) {
-            console.log('valueLength is > 0. so I will add freeze');
-            label.addClass("freeze");
-        } else {
-            label.removeClass("freeze");
+    });
+
+
+
+
+
+
+    /*Form*/
+
+    var formGroups = $( ".form-group" );
+
+
+    formGroups.each( function () {
+        var formGroup = $( this );
+        var label = formGroup.find( 'label' );
+        var textareaSelection = formGroup.find( 'textarea' );
+        var generalInput = formGroup.find( 'input' ).add( textareaSelection ); /*selection*/
+        console.log( "generalInput is " );
+        console.log( generalInput );
+
+        function checkInput() {
+
+            console.log( "generalInput.val() is " + generalInput.val() );
+
+            var valueLength = generalInput.val().length;
+
+
+            console.log( "generalInput value length is: " + valueLength );
+
+            if ( valueLength > 0 ) {
+                console.log( 'valueLength is > 0. so I will add freeze' );
+                label.addClass( "freeze" );
+            } else {
+                label.removeClass( "freeze" );
+            }
+
         }
 
-    }
 
-    input.change(function(){
-        console.log("checkInput part");
-        checkInput();
-        });
-    });
-});
 
+        generalInput.change( function () {
+            console.log( "checkInput part" );
+            checkInput();
+        } ); /*generalInput*/
+
+    } ); /*form-group each*/
+
+} ); /*document ready)*/
